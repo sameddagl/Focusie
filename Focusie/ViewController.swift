27010b5cf9e792931454ffb29653a14195a337c4
sameddagl <samedddagl@gmail.com>
2022-11-29 20:52:23 +0300

    Initial Commit

---
+++ Focusie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Focusie
+//
+//  Created by Samed Dağlı on 29.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
